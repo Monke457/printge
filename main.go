9d7f8e4473e215ff2b30b35052e0980192b4f17e
Beth <beth.williams@bluewin.ch>
2025-02-19 16:12:13 +0100

    holy moly

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
-package main
+package printge
 
-func main() {
-	//todo: everything
+import "fmt"
+
+type Color string 
+
+const (
+	DEFAULT Color = "\033[0;39m"
+	BLACK = "\033[0;30m"
+	DARKRED = "\033[0;31m"
+	DARKGREEN = "\033[0;32m"
+	DARKYELLOW = "\033[0;33m"
+	DARKBLUE = "\033[0;34m"
+	DARKMAGENTA = "\033[0;35m"
+	DARKCYAN = "\033[0;36m"
+	GRAY = "\033[0;37m"
+	DARKGRAY = "\033[1;90m"
+	RED = "\033[1;91m"
+	GREEN = "\033[1;92m"
+	YELLOW = "\033[1;93m"
+	BLUE = "\033[1;94m"
+	MAGENTA = "\033[1;95m"
+	CYAN = "\033[1;96m"
+	WHITE = "\033[1;97m"
+)
+
+func Print(content string, color Color) {
+	fmt.Printf("%s%s%s", color, content, DEFAULT)
 }
